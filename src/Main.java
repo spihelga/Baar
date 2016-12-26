bbab6011a7a009955d75fd6d0c4f42884e24c3ea
Spihelga <Sander.pihelgas@itcollege.ee>
2016-12-27 00:35:28 +0200

    GUI algus

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,61 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main extends Application {
+    Stage window;
+    Button Alusta;
+    Scene Avaaken, stockaken;
+
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Tudengibaar");
+
+        window = new Stage();
+        VBox Aken1 = new VBox(20);
+        Aken1.setPadding(new Insets(10, 10, 10, 10));
+
+        //Tekst kastis
+        Label Avatekst = new Label("See programm aitab sul kokteile segada");
+        Aken1.setAlignment(Pos.CENTER);
+
+        //Alustamise nupp
+        Alusta = new Button("Alusta");
+        Alusta.setOnAction(e -> Stock.display());
+
+        Aken1.getChildren().addAll(Avatekst, Alusta);
+        Avaaken = new Scene(Aken1, 300, 120);
+
+        //Teine aken
+        GridPane Aken2 = new GridPane();
+
+        stockaken = new Scene(Aken2);
+
+        primaryStage.setScene(Avaaken);
+        primaryStage.show();
+    }
+
+
+
+
+
+
+
+    public static void Andmed(String[] args) {
         System.out.println("Kui palju rummi sul on? (L)");
         Scanner srum = new Scanner(System.in);
         double ruml = srum.nextDouble();
--- src/Stock.java
+++ src/Stock.java
@@ -1,7 +1,21 @@
-package PACKAGE_NAME;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * Created by Sander on 26.12.2016.
  */
-public class Stock {
+public class Stock {                        //Siia aknasse tulevad lahtrid ja jookide algsed kogused
+
+    public static void display() {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        GridPane Aken2 = new GridPane();
+        Aken2.getChildren().addAll();
+
+        Scene stockaken = new Scene(Aken2);
+        window.setScene(stockaken);
+        window.show();
+         }
 }
